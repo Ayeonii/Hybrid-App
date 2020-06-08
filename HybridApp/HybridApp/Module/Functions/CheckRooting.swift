bd7b7613c5005e64201477bd60208393a3045eaf
Ayeon <dldkekdusz@naver.com>
2020-06-08 18:45:27 +0900

    Master 수정

---
+++ HybridApp/HybridApp/Module/Functions/CheckRooting.swift
@@ -0,0 +1,90 @@
+//
+//  CheckRooting.swift
+//  HybridApp
+//
+//  Created by 이아연 on 2020/05/31.
+//  Copyright © 2020 Ayeon. All rights reserved.
+//
+
+import UIKit
+import FlexHybridApp
+
+/*
+ 권한 / 루팅체킹 모듈
+ */
+class CheckRooting {
+    
+    let util = Utils()
+    
+    func checkRootingFunction (_ currentVC : UIViewController) -> ((Array<Any?>) -> Any?) {
+        return{ (arguments) -> String in
+            
+            self.util.setUserHistory(forKey: "RootingCheckBtn")
+            
+            var returnStr : String = "Not Root Authority"
+            DispatchQueue.main.async{
+                if !self.hasJailbreak() {
+                    returnStr = "RootAuthority"
+                    let dialog = UIAlertController(title: nil, message: "루트권한을 가진 디바이스에서는 실행할 수 없습니다. 앱이 종료됩니다.", preferredStyle: .alert)
+                    let action = UIAlertAction(title: "확인", style: .default){
+                        (action:UIAlertAction!) in
+                        exit(0)
+                    }
+                    dialog.addAction(action)
+                    currentVC.present(dialog, animated: true, completion: nil)
+                }
+            }
+            return returnStr
+        }
+    }
+    
+    func hasJailbreak() -> Bool {
+        
+        guard let cydiaUrlScheme = NSURL(string: "cydia://package/com.example.package") else { return false }
+        if UIApplication.shared.canOpenURL(cydiaUrlScheme as URL) {
+            return true
+        }
+        
+        #if arch(i386) || arch(x86_64)
+        return false
+        #endif
+        
+        //file check
+        let fileManager = FileManager.default
+        if fileManager.fileExists(atPath: "/Applications/Cydia.app") ||
+            fileManager.fileExists(atPath: "/Library/MobileSubstrate/MobileSubstrate.dylib") ||
+            fileManager.fileExists(atPath: "/bin/bash") ||
+            fileManager.fileExists(atPath: "/usr/sbin/sshd") ||
+            fileManager.fileExists(atPath: "/etc/apt") ||
+            fileManager.fileExists(atPath: "/usr/bin/ssh") ||
+            fileManager.fileExists(atPath: "/private/var/lib/apt") {
+            return true
+        }
+        
+        //rootAutority check
+        if canOpen(path: "/Applications/Cydia.app") ||
+            canOpen(path: "/Library/MobileSubstrate/MobileSubstrate.dylib") ||
+            canOpen(path: "/bin/bash") ||
+            canOpen(path: "/usr/sbin/sshd") ||
+            canOpen(path: "/etc/apt") ||
+            canOpen(path: "/usr/bin/ssh") {
+            return true
+        }
+        let path = "/private/" + NSUUID().uuidString
+        do {
+            try "anyString".write(toFile: path, atomically: true, encoding: String.Encoding.utf8)
+            try fileManager.removeItem(atPath: path)
+            return true
+        } catch {
+            return false
+        }
+    }
+    
+    func canOpen(path: String) -> Bool {
+        let file = fopen(path, "r")
+        guard file != nil else { return false }
+        fclose(file)
+        return true
+    }
+}
+
---
+++ HybridApp/HybridApp/Module/Functions/Notification.swift
@@ -0,0 +1,72 @@
+//
+//  Notification.swift
+//  HybridApp
+//
+//  Created by 이아연 on 2020/05/31.
+//  Copyright © 2020 Ayeon. All rights reserved.
+//
+
+import UIKit
+import FlexHybridApp
+import UserNotifications
+
+/*
+ 노티피케이션
+ */
+class Notification: NSObject, UNUserNotificationCenterDelegate {
+    
+    private let currentVC : UIViewController
+    private let util = Utils()
+    
+    init(_ currentVC : UIViewController){
+        self.currentVC = currentVC
+    }
+    
+    func notifiFunction () -> ( Array<Any?>) -> Any?{
+        return {(arguments) -> String in
+            
+            self.util.setUserHistory(forKey: "NotificationBtn")
+            
+            let data = Data("Array<Any?>?".utf8)
+            print(data)
+            let content = UNMutableNotificationContent()
+            if let recieveData = arguments[0] as? Dictionary< String , Any?>
+            {
+                content.title = recieveData["title"] as? String ?? ""
+                content.subtitle = recieveData["subTitle"] as? String ?? ""
+                content.body = recieveData["message"] as? String ?? ""
+                content.badge = NSNumber(value: recieveData["badge"] as? Int ?? 0)
+                
+                let identifier = recieveData["identifier"] as? String ?? "default"
+                let isRepeat = recieveData["isRepeat"] as? Bool ?? false
+                var sec = recieveData["duration"] as! Double
+                if sec <= 0{
+                    sec = 0.01
+                }
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: sec , repeats:isRepeat)
+                let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+                let center = UNUserNotificationCenter.current()
+                
+                center.delegate = self
+                center.add(request, withCompletionHandler: nil)
+                
+                return "Notification Activate!"
+            }else {
+                return "Parameter "
+            }
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, openSettingsFor notification: UNNotification?) {
+        self.currentVC.present(self.currentVC, animated: true, completion: nil)
+    }
+    
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent
+        notification: UNNotification, withCompletionHandler completionHandler:
+        @escaping (UNNotificationPresentationOptions) -> Void) {
+
+        return completionHandler(UNNotificationPresentationOptions.alert)
+    }
+}
